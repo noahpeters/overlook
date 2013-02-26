dc77f75a295ab2998fa727c0b8b79d90e754bf1a
noah <noahpeters@gmail.com>
2013-02-25 20:32:10 -0500

    added excluded processes.js

---
+++ processes.js
@@ -0,0 +1,17 @@
+var exec = require('child_process').exec;
+var processes = function (callback) {
+    exec('ps o pid= o command= | sed -e "s/^[ \t]*//"', function(err, stdout, stderr) {
+        // stdout is a string containing the output of the command.
+        // parse it and look for the apache and mysql processes.
+        //callback(err, stdout, stderr);
+        var pl = stdout;
+        var procs = pl.split("\n");
+        var obj = { };
+        procs.forEach(function (proc) {
+            var cols = proc.split(" ");
+            obj[cols.shift()] = cols.join(" ");
+        });
+        callback(obj);
+    });
+};
+module.exports = processes;
